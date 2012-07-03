e632796fa721fcdd6ecb565c20237dcc1640c232
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-07-03 22:21:57 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneWithRails::Application.config.secret_token = 'a2db7c6da435badfaa75db33174159012806adadfb808bdfa6f5de93088fc57978bed5d39aa4b0a4114786286aa5dccb3f168ade0d183d35f611150b993f6d7a'
